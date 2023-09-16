2bdf5c3c95cb931260fccd9efe24381df32d7753
Thanh Sơn <106719620+ThanhSon02@users.noreply.github.com>
2023-09-16 08:49:17 +0700

    Change server url

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:3001/api/v1",
-    baseURL: "https://ts-book-store-server.onrender.com/api/v1",
+    // baseURL: "https://ts-book-store-server.onrender.com/api/v1",
+    baseURL: "https://book-store-server-ten.vercel.app/api/v1",
     headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Credentials": true,
